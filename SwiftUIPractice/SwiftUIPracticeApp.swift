d5f28ca14dd4123fa87db88c6a3c8546f93707d8
Aleksey Smitskiy <smickiy.aleksey@adv.ru>
2022-04-17 16:27:58 +0300

    first commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Алексей Смицкий on 17.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
